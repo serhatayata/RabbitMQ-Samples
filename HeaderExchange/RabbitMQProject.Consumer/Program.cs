584d5c7abeb682e47c9e8740fa51a210cbac2b5a
serhat.ayata <serhat.ayata@OBL-COM-0331>
2022-12-05 16:51:21 +0300

    HeaderExchanged added

---
+++ HeaderExchange/RabbitMQProject.Consumer/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Shared;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory()
+{
+    Port = 5672
+};
+
+factory.UserName = "guest";
+factory.Password = "guest";
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+Dictionary<string, object> headers = new();
+
+headers.Add("format", "pdf");
+//headers.Add("shape2", "a4");
+headers.Add("shape", "a4");
+headers.Add("x-match", "all"); //All of them has to match
+//headers.Add("x-match", "any"); //Just one of them has to match
+
+//We use QueueBind so that when the consumer was closed then the queue will be gone.
+channel.QueueBind(queueName, "header-exchange", String.Empty, headers);
+
+channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine("Logs listening ...");
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    var product = JsonSerializer.Deserialize<Product>(message);
+
+    Thread.Sleep(1500);
+    Console.WriteLine($"Message : {product.Id} - {product.Name} - {product.Price} - {product.Stock}");
+
+
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+Console.ReadLine();
---
+++ HeaderExchange/RabbitMQProject.Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using Shared;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory()
+{
+    Port = 5672
+};
+
+factory.UserName = "guest";
+factory.Password = "guest";
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "header-exchange", durable: true, type: ExchangeType.Headers);
+
+Dictionary<string, object> headers = new();
+
+headers.Add("format", "pdf");
+headers.Add("shape", "a4");
+
+var properties = channel.CreateBasicProperties();
+properties.Headers = headers;
+properties.Persistent = true; //Messages will be persistent even if rabbitmq is restarted... This can be used with any other exchanges
+
+var product = new Product() { Id = 1, Name = "Pencil", Price = 25, Stock = 100 };
+
+var productJsonString = JsonSerializer.Serialize(product);
+
+channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes(productJsonString));
+
+Console.ReadLine();
