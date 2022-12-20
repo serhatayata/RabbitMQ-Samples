3bcfc75f77fef3750b8c121eee6de63e870d0069
serhat.ayata <serhat.ayata@OBL-COM-0331>
2022-12-20 15:34:08 +0300

    DbContext and seed data completed

--- RabbitMQProjects.ExcelApp/RabbitMQProjects.ExcelCreateApp/Data/AppDbContext.cs
+++ RabbitMQProjects.ExcelApp/RabbitMQProjects.ExcelCreateApp/Data/AppDbContext.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using RabbitMQProjects.ExcelCreateApp.Models;
 
 namespace RabbitMQProjects.ExcelCreateApp.Data
 {
     public class AppDbContext:IdentityDbContext
     {
+        public AppDbContext()
+        {
+
+        }
+
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
 
@@ -14,5 +20,14 @@ namespace RabbitMQProjects.ExcelCreateApp.Data
 
         public DbSet<UserFile> UserFiles { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=OBL-COM-0331;Initial Catalog=RabbitMQProjectExcel_Identity;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
     }
+
+ 
 }
--- RabbitMQProjects.ExcelApp/RabbitMQProjects.ExcelCreateApp/Program.cs
+++ RabbitMQProjects.ExcelApp/RabbitMQProjects.ExcelCreateApp/Program.cs
@@ -1,9 +1,39 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using RabbitMQProjects.ExcelCreateApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(opt =>
+{
+    opt.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<AppDbContext>();
+
+
 
 var app = builder.Build();
+#region SEED DATA
+using (var scope = app.Services.CreateScope())
+{
+    var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    appDbContext.Database.Migrate();
+    if (!appDbContext.Users.Any())
+    {
+        userManager.CreateAsync(new IdentityUser() { UserName = "deneme", Email = "deneme@outlook.com" }, "Password12*").Wait();
+        userManager.CreateAsync(new IdentityUser() { UserName = "deneme2", Email = "deneme2@outlook.com" }, "Password12*").Wait();
+    }
+}
+#endregion
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,7 +47,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAuthorization();
 
 app.MapControllerRoute(
